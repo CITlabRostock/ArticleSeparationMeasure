9f1439f03336e99757d7539192f56cf03cf61e32
Max Weidemann <max.weidemann499@gmail.com>
2018-12-12 13:01:44 +0100

    Added the poly_to_string method, which is not taken into account the region polygons (see Java code). Added a test method for the poly_to_string method. Changed the load_text_file method, s.t. empty lines are not taken into account.

--- util/util.py
+++ util/util.py
@@ -3,20 +3,27 @@ from geometry import Polygon
 
 
 def load_text_file(filename):
-    """Load text file ``filename`` and return the lines as """
+    """Load text file ``filename`` and return the (stripped) lines as list entries.
+
+    :param filename: path to the file to be loaded
+    :type filename: str
+    :return: list of strings consisting of the (stripped) lines from filename
+    """
+
     res = []
     with open(filename, 'r') as f:
         for line in f:
-            res.append(line.strip())
+            if not line.isspace():
+                res.append(line.strip())
         return res
 
 
 def parse_string(string_polygon):
     """Parse the polygon represented by the string ``string_polygon`` and return a ``Polygon`` object.
 
-    :param string_polygon: coordinates of a polygon given in string format: x1,y1;x2,y2;...;xn,yn.
+    :param string_polygon: coordinates of a polygon given in string format: x1,y1;x2,y2;...;xn,yn
     :type string_polygon: str
-    :return: Polygon object with the coordinates given in string_polygon.
+    :return: Polygon object with the coordinates given in string_polygon
     """
 
     polygon = Polygon()
@@ -33,3 +40,58 @@ def parse_string(string_polygon):
     return polygon
 
 
+def poly_to_string(polygon):
+    """Inverse method of ``parse_string``, taking a polygon as input and outputs a string holding the x,y coordinates
+    of the points present in the polygon separated by semicolons ";".
+
+    :param polygon: input polygon to be parsed
+    :type polygon: Polygon
+    :return: a string holding the x,y coordinates of the polygon in format: x1,y1;x2,y2;...;xn,yn
+    """
+
+    res = ""
+    for x, y in zip(polygon.x_points, polygon.y_points):
+        if len(res) != 0:
+            res += ";"
+        res += str(x) + "," + str(y)
+    return res
+
+
+# get_polys_from_page_file not necessary since we're only handling strings which are produced from Java routines
+
+def get_polys_from_file(poly_file_name):
+    """Load polygons from a text file ``poly_file_name`` and save them as ``Polygon`` objects in a list.
+
+    :param poly_file_name: path to the txt file holding the polygons (one polygon per line)
+    :type poly_file_name: str
+    :return: a tuple containing the list of polygons and a boolean value representing if the polygons are loaded with
+    errors
+    """
+
+    # TODO: Bool return value necessary? -> Just check if returned list is None (then you know if it was skipped or not)
+    poly_strings = load_text_file(poly_file_name)
+    if len(poly_strings) == 0:
+        return None, False
+
+    res = []
+    for poly_string in poly_strings:
+        try:
+            poly = parse_string(str(poly_string))
+            res.append(poly)
+        except ValueError:
+            return None, True
+    return res, False
+
+
+def norm_des_dist(poly_list, des_dist):
+    """
+
+    :param poly_list: list of polygons
+    :param des_dist:
+    :type poly_list: list
+    :type des_dist: int
+    :return: list of polygons
+    """
+
+    res = []
+    pass
